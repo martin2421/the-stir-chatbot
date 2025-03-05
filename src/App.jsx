4cfaeb441908aee24db0aae0ce2e3fa3219c5043
Dovakiin7 <hamzak477777@gmail.com>
2025-03-04 17:04:11 -0800

    Updated font
    new fonts

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
             <div className="chat-box-header">
               <span className="chat-box-toggle"><button className="material-icons">&times;</button></span>
 
-              <p>Stir Food Chatbot</p>
+              <p>Stir Food Assisstant</p>
               <button id="clearHistoryButton">&#x27f3;</button>
 
             </div>
